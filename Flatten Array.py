2f6ec63057a41dee534cc2bcb54d1e6b34f6daf7
granitelegend <83376235+granitelegend@users.noreply.github.com>
2022-11-15 07:04:11 +0100

    Removed comments, remade as function

--- Flatten Array.py
+++ Flatten Array.py
@@ -1,62 +1,69 @@
 
+# How it works:
 # iter1 = [1,2,a,b,3,4]
 # iter2 = [12,a,b,3,4]
 # iter5 = [12,a,b,34]
 
-# Process never stops if nothing can be processed
+# Examples of working inputs:
+# arg = ['1','2','a','b','3','4','5','c','',']']
+# arg = [1,[2,355,5,None,4],[None],5,4,4,44]
 
-arg = ['1','2','a','b','3','4','5','c','',']']
-arg = list(str([1,[2,355,5,None,4],[None],5]))
+def horrible_flatten_array(arg):
+    """
+    This flatten array function could be deemed 'horrible' due to its primitive method of flattening it.
+    Function converts list -> string -> list, creating a large list, numbers that are close to each other are merged, -
+    - and it is done this way to preserve numbers greater than 9 in the original list.
+    Final step is removing all non-numbers in the list and returning it.
+    :return:
+    """
 
-while ' ' in arg:
-    arg.remove(' ')
+    arg = list(str(arg[:]))
 
-print(arg)
+    while ' ' in arg:
+        arg.remove(' ')
 
-arg_numbers_only = []
+    arg_numbers_only = []
 
-for x in arg:
-    if x.isnumeric():
-        arg_numbers_only.append(int(x))
+    for x in arg:
+        if x.isnumeric():
+            arg_numbers_only.append(int(x))
 
-digit_list = list(range(0,max(arg_numbers_only)+1))
-digitsString = ''
+    digit_list = list(range(0,max(arg_numbers_only)+1))
+    digitsString = ''
 
-print(digit_list)
+    for x in digit_list:
+        digitsString += str(x)
 
-for x in digit_list:
-    digitsString += str(x)
+    digit_list = list(digitsString)
 
-digit_list = list(digitsString)
+    pre_arg = 0
+    post_arg = 1
 
-pre_arg = 0
-post_arg = 1
-
-while pre_arg != post_arg:
-    pre_arg = arg[:]
-    new_number = ''
-    for y in range(len(arg)):
-        try:
-            if (arg[y] in digit_list) and (arg[y+1] in digit_list):
-                print((arg[y] in digit_list) and (arg[y+1] in digit_list), arg[y], arg[y+1])
-                new_number = arg[y] = arg[y] + arg[y+1]
-                arg.pop(y+1)
-                if not (new_number in digit_list):
-                    digit_list.append(str(new_number))
+    while pre_arg != post_arg:
+        pre_arg = arg[:]
+        new_number = ''
+        for y in range(len(arg)):
+            try:
+                if (arg[y] in digit_list) and (arg[y+1] in digit_list):
+                    new_number = arg[y] = arg[y] + arg[y+1]
+                    arg.pop(y+1)
+                    if not (new_number in digit_list):
+                        digit_list.append(str(new_number))
+                    else:
+                        continue
+                    break
                 else:
                     continue
+            except IndexError:
                 break
-            else:
-                continue
-        except IndexError:
-            break
 
-    post_arg = arg[:]
+        post_arg = arg[:]
 
-new_arg = []
+    new_arg = []
 
-for x in arg:
-    if x.isnumeric():
-        new_arg.append(x)
+    for x in arg:
+        if x.isnumeric():
+            new_arg.append(x)
 
-print(new_arg)
+    return new_arg
+
